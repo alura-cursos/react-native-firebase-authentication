e9dc0719845e3a6a0950dfde1d45484ab4653c94
andreocunha <andreoliveiracunha20@gmail.com>
2022-08-09 17:31:21 -0300

    aula 2.2

--- src/servicos/requisicoesFirebase.js
+++ src/servicos/requisicoesFirebase.js
@@ -1,12 +1,34 @@
 import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
 
-export async function cadastrar(email, senha, confirmaSenha) {
-  createUserWithEmailAndPassword(auth, email, senha)
+function errosFirebase(error){
+  let mensagem = '';
+  switch(error.code) {
+    case AuthErrorCodes.EMAIL_EXISTS:
+      mensagem = "Esse email já está em uso";
+      break;
+    case AuthErrorCodes.INVALID_EMAIL:
+      mensagem = "Email inválido";
+      break;
+    case AuthErrorCodes.WEAK_PASSWORD:
+      mensagem = "A senha precisa de no minimo 6 caracteres";
+      break;
+    default:
+      mensagem="Erro desconhecido";
+  }
+  return mensagem;
+}
+
+export async function cadastrar(email, senha) {
+  const resultado = await createUserWithEmailAndPassword(auth, email, senha)
   .then((dadosDoUsuario) => {
     console.log(dadosDoUsuario)
+    return "sucesso"
   })
   .catch((error) => {
-    console.log(error)
+    console.log(error);
+    return errosFirebase(error)
   });
+
+  return resultado
 }
--- src/telas/Cadastro/index.js
+++ src/telas/Cadastro/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { Alert, View } from 'react-native';
 import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import estilos from './estilos';
@@ -10,12 +10,36 @@ export default function Cadastro({ navigation }) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [confirmaSenha, setConfirmaSenha] = useState('');
+  const [statusError, setStatusError] = useState('');
+  const [mensagemError, setMensagemError] = useState('');
 
   async function realizarCadastro(){
-    await cadastrar(email, senha, confirmaSenha);
-    setEmail('')
-    setSenha('')
-    setConfirmaSenha('')
+    if(email == ''){
+      setMensagemError('Preencha com seu email');
+      setStatusError('email');
+    } else if(senha == ''){
+      setMensagemError('Digite sua senha');
+      setStatusError('senha');
+    } else if(confirmaSenha == ''){
+      setMensagemError('Confirme sua senha');
+      setStatusError('confirmaSenha');
+    } else if(confirmaSenha != senha){
+      setMensagemError('As senhas não conferem!');
+      setStatusError('confirmaSenha');
+    } else {
+      const resultado = await cadastrar(email, senha);
+      if(resultado == 'sucesso') {
+        Alert.alert('Usuário cadastrado com sucesso!');
+        setEmail('')
+        setSenha('')
+        setConfirmaSenha('')
+      }
+      else {
+        Alert.alert(resultado);
+      }
+      setStatusError('')
+      setMensagemError('')
+    }
   }
 
   return (
@@ -24,12 +48,16 @@ export default function Cadastro({ navigation }) {
         label="E-mail"
         value={email}
         onChangeText={texto => setEmail(texto)}
+        error={statusError == 'email'}
+        messageError={mensagemError}
       />
       <EntradaTexto
         label="Senha"
         value={senha}
         onChangeText={texto => setSenha(texto)}
         secureTextEntry
+        error={statusError == 'senha'}
+        messageError={mensagemError}
       />
 
       <EntradaTexto
@@ -37,6 +65,8 @@ export default function Cadastro({ navigation }) {
         value={confirmaSenha}
         onChangeText={texto => setConfirmaSenha(texto)}
         secureTextEntry
+        error={statusError == 'confirmaSenha'}
+        messageError={mensagemError}
       />
       
       <Botao onPress={() => realizarCadastro()}>CADASTRAR</Botao>
