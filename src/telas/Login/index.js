c28b09fb81d588d777b9e9e6afc00f4ea21b3aaf
andreocunha <andreoliveiracunha20@gmail.com>
2022-08-09 18:53:55 -0300

    aula 3.2

--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -4,14 +4,31 @@ import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import { logar } from '../../servicos/requisicoesFirebase';
 import estilos from './estilos';
+import { Alerta } from '../../componentes/Alerta';
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [statusError, setStatusError] = useState('');
+  const [mensagemError, setMensagemError] = useState('');
 
   async function realizarLogin(){
-    const resultado = await logar(email, senha);
-    console.log(resultado)
+    if(email == ''){
+      setMensagemError('O email é obrigatório!');
+      setStatusError('email');
+    } else if(senha == ''){
+      setMensagemError('A senha é obrigatória!');
+      setStatusError('senha');
+    } else {
+      const resultado = await logar(email, senha);
+      if(resultado == 'erro'){
+        setStatusError('firebase')
+        setMensagemError('Email ou senha não conferem')
+      } 
+      else {
+        navigation.navigate('Principal')
+      }
+    }
   }
 
   return (
@@ -20,12 +37,22 @@ export default function Login({ navigation }) {
         label="E-mail"
         value={email}
         onChangeText={texto => setEmail(texto)}
+        error={statusError == 'email'}
+        messageError={mensagemError}
       />
       <EntradaTexto
         label="Senha"
         value={senha}
         onChangeText={texto => setSenha(texto)}
         secureTextEntry
+        error={statusError == 'senha'}
+        messageError={mensagemError}
+      />
+
+      <Alerta 
+        mensagem={mensagemError}
+        error={statusError == 'firebase'}
+        setError={setStatusError}
       />
       
       <Botao onPress={() => realizarLogin()}>LOGAR</Botao>
