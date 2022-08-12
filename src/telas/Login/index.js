f32479a338beb29feedad28adb7e72edc44426e0
andreocunha <andreoliveiracunha20@gmail.com>
2022-08-11 22:11:55 -0300

    aula 4.2

--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -1,16 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
 import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import { logar } from '../../servicos/requisicoesFirebase';
 import estilos from './estilos';
 import { Alerta } from '../../componentes/Alerta';
+import { auth } from '../../config/firebase';
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
   const [statusError, setStatusError] = useState('');
   const [mensagemError, setMensagemError] = useState('');
+
+  useEffect(() => {
+    const estadoUsuario = auth.onAuthStateChanged( usuario => {
+      if(usuario){
+        navigation.replace('Principal')
+      }
+    })
+    return () => estadoUsuario();
+  },[])
 
   async function realizarLogin(){
     if(email == ''){
